e8e69658cb9f6451cd76c87d8324acc285ec8a50
Christian Heckendorf <heckendorfc@gmail.com>
2021-07-01 10:37:13 -0400

    initial client

---
+++ R/api.R
@@ -0,0 +1,34 @@
+endpoint="https://www.bu.edu/dbin/cnsb/CETSA/api.php"
+
+list.projects = function(){
+	res=httr::GET(endpoint,query=list(q="project"))
+	httr::content(res)
+}
+
+demo.project = function(){
+	list(name="new project",lab="some lab",project_type="what is a type?")
+}
+
+add.project = function(data){
+	if(is.null(data)){
+		data=demo.project()
+		#todo error?
+	}
+	res=httr::POST(endpoint,body=list(q="project",data=data),encode="json")
+	#print(res)
+	httr::content(res)
+}
+
+demo.experiment = function(){
+	list(projectID=1,experiment_type="what is a type?",done_by="Ryan",num_samples=2,num_replicates=2,assay_type="what is this?",concentration_range="1:10",temperature_range="1:10",organism="Human",run_date="2020-06-29") # TODO: proteingroups / drug
+}
+
+add.experiment = function(data){
+	if(is.null(data)){
+		data=demo.experiment()
+		#todo error?
+	}
+	res=httr::POST(endpoint,body=list(q="experiment",data=data),encode="json")
+	#print(res)
+	httr::content(res)
+}
